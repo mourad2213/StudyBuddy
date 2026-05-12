7260d553914f47904a84c7feb1f79b5ae3175d3e
mourad2213 <ahmed.mourad338@gmail.com>
2026-05-12 21:04:25 +0300

    Merging Rahma2 to mourad

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,81 +1,119 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  HttpLink,
+} from "@apollo/client";
+
 import { ApolloProvider } from "@apollo/client/react";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { from } from "@apollo/client";
+
 import App from "./App";
 import "./index.css";
 
-// Service URLs
+/* =========================
+   Service URLs
+========================= */
 const userServiceUrl = "http://localhost:4001/graphql";
-
-const sessionServiceUrl = "http://localhost:4007/graphql";
 const profileServiceUrl = "http://localhost:4006/graphql";
+const sessionServiceUrl = "http://localhost:4007/graphql";
 const notificationServiceUrl = "http://localhost:4005/graphql";
 
-// Create links for different services
-const userServiceLink = createHttpLink({ uri: userServiceUrl });
-const sessionServiceLink = createHttpLink({ uri: sessionServiceUrl });
-const notificationServiceLink = createHttpLink({ uri: notificationServiceUrl });
-const profileServiceLink = createHttpLink({ uri: profileServiceUrl });
-
-// Router link - direct operations to the correct service
-import { ApolloLink } from "@apollo/client";
-
-const routerLink = new ApolloLink((operation, forward) => {
-  const { operationName } = operation;
-  
-  // Session service operations - use correct operation names from GraphQL queries/mutations
-  const sessionOperations = [
-    'CreateStudySession',
-    'RespondToSessionInvitation',
-    'JoinSession',
-    'LeaveSession',
-    'CancelSession',
-    'GetUpcomingSessions',
-    'GetPastSessions',
-    'GetSessionAcceptedMembers',
-    'GetPendingInvitations',
-    'GetSessionById',
-  ];
-
-  // Notification service operations
-  const notificationOperations = [
-    'GetNotifications',
-    'GetUnreadNotifications',
-    'GetUnreadCount',
-    'MarkAsRead',
-    'onMarkAsRead',
-    'MarkAllAsRead',
-    'DeleteNotification',
-    'UpdateNotificationMessage',
-  ];
-
-  if (sessionOperations.includes(operationName)) {
-    operation.setContext({ uri: sessionServiceUrl });
-    return sessionServiceLink.request(operation, forward);
-  }
-
-  if (notificationOperations.includes(operationName)) {
-    operation.setContext({ uri: notificationServiceUrl });
-    return notificationServiceLink.request(operation, forward);
-  }
-  
-    operation.setContext({ uri: profileServiceUrl });
-    return profileServiceLink.request(operation, forward);
-  
-
-  // Default to user service
-  operation.setContext({ uri: userServiceUrl });
-  return userServiceLink.request(operation, forward);
+/* =========================
+   HTTP Links
+========================= */
+const userServiceLink = new HttpLink({
+  uri: userServiceUrl,
 });
 
+const profileServiceLink = new HttpLink({
+  uri: profileServiceUrl,
+});
+
+const sessionServiceLink = new HttpLink({
+  uri: sessionServiceUrl,
+});
+
+const notificationServiceLink = new HttpLink({
+  uri: notificationServiceUrl,
+});
+
+/* =========================
+   Operation Names
+========================= */
+
+// Session Service Operations
+const sessionOperations = [
+  "CreateStudySession",
+  "RespondToSessionInvitation",
+  "JoinSession",
+  "LeaveSession",
+  "CancelSession",
+  "GetUpcomingSessions",
+  "GetPastSessions",
+  "GetSessionAcceptedMembers",
+  "GetPendingInvitations",
+  "GetSessionById",
+];
+
+// Notification Service Operations
+const notificationOperations = [
+  "GetNotifications",
+  "GetUnreadNotifications",
+  "GetUnreadCount",
+  "MarkAsRead",
+  "onMarkAsRead",
+  "MarkAllAsRead",
+  "DeleteNotification",
+  "UpdateNotificationMessage",
+];
+
+// Profile Service Operations
+const profileOperations = [
+  "GetProfile",
+  "GetProfileByUserId",
+  "UpdateProfile",
+  "CreateProfile",
+];
+
+/* =========================
+   Router Link
+========================= */
+const routerLink = new ApolloLink((operation) => {
+  const { operationName } = operation;
+
+  // SESSION SERVICE
+  if (sessionOperations.includes(operationName)) {
+    return sessionServiceLink.request(operation);
+  }
+
+  // NOTIFICATION SERVICE
+  if (notificationOperations.includes(operationName)) {
+    return notificationServiceLink.request(operation);
+  }
+
+  // PROFILE SERVICE
+  if (profileOperations.includes(operationName)) {
+    return profileServiceLink.request(operation);
+  }
+
+  // DEFAULT -> USER SERVICE
+  return userServiceLink.request(operation);
+});
+
+/* =========================
+   Apollo Client
+========================= */
 const client = new ApolloClient({
-  link: createHttpLink({ uri: sessionServiceUrl }),
+  link: routerLink,
   cache: new InMemoryCache(),
 });
 
+/* =========================
+   Render App
+========================= */
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
